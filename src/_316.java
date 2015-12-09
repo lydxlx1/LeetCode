86f940b121dd9e1cfc8f9331c1533b5331015435
Yuan Li <lydxlx@gmail.com>
2015-12-09 16:21:56 -0600

    Code shortened, 316 - Remove Duplicate Letters

--- src/_316.java
+++ src/_316.java
@@ -1,34 +1,28 @@
 /**
  * LeetCode 316 - Remove Duplicate Letters
- *
+ * <p>
  * An O(n) greedy solution
- *
+ * <p>
  * Given the string s, the greedy choice (the leftmost letter in the answer) is the smallest s[i], s.t.
  * the suffix s[i .. ] contains all the unique letters.
- *
+ * <p>
  * After determining the greedy choice s[i], we get a new string s' by
  * 1) removing all letters to the left of s[i],
  * 2) removing all s[i]'s from s.
- *
+ * <p>
  * We then recursively solve the problem w.r.t. s'.
  * The runtime is O(26 * n) = O(n).
  */
 public class _316 {
     public String removeDuplicateLetters(String s) {
         int[] cnt = new int[26];
-        boolean[] isCandidate = new boolean[26];
+        int pos = 0;
         for (int i = 0; i < s.length(); i++) cnt[s.charAt(i) - 'a']++;
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            isCandidate[ch - 'a'] = true;
+            if (ch < s.charAt(pos)) pos = i;
             if (--cnt[ch - 'a'] == 0) break;
         }
-        for (int i = 0; i < 26; i++)
-            if (isCandidate[i]) {
-                char ch = (char) (i + 'a');
-                for (int j = 0; j < s.length(); j++)
-                    if (s.charAt(j) == ch) return ch + removeDuplicateLetters(s.substring(j + 1).replaceAll("" + ch, ""));
-            }
-        return "";
+        return s.length() == 0 ? "" : s.charAt(pos) + removeDuplicateLetters(s.substring(pos + 1).replaceAll("" + s.charAt(pos), ""));
     }
 }
