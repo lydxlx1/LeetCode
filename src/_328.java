e91d24c1ff4ea1e1db5f29c352509aacc51a4486
Yuan Li <lydxlx@gmail.com>
2016-01-16 22:42:07 -0600

    Add 328, Odd Even Linked List, O(n) in-place solution

---
+++ src/_328.java
@@ -0,0 +1,31 @@
+/**
+ * ListCode 328 - Odd Even Linked List
+ *
+ * O(n) in-place solution
+ */
+public class _328 {
+    public ListNode oddEvenList(ListNode head) {
+        ListNode odd = head, even = odd == null ? null : odd.next, oddTail = odd;
+        for (ListNode i = odd, j = even; i != null && j != null; )
+            if (i.next == j) {
+                i.next = j.next;
+                i = j.next;
+                if (i != null) oddTail = i;
+            } else {
+                j.next = i.next;
+                j = i.next;
+            }
+        if (oddTail != null) oddTail.next = even;
+        return odd;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+        next = null;
+    }
+}
