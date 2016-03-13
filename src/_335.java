22e9491d9a761f40564c096785ca69a3622c45a0
Yuan Li <lixx2100@umn.edu>
2016-03-12 19:30:40 -0600

    Add problem 335, 337

---
+++ src/_335.java
@@ -0,0 +1,39 @@
+/**
+ * LeetCode 335 - Self Crossing
+ *
+ * Geometry
+ *
+ * Based on how the spiral is define, there are three cases to have intersection.
+ *
+ *                                      ^------------>
+ *                                      |            |
+ *    ^                                 |            |
+ *    |                                 |            |
+ * *--+---->       ^--->  *---->        |      *-----+---->
+ *    |    |       |           |        |            |    |
+ *    |    |       |           |        |            v    |
+ *    |    |       |           |        |                 |
+ *    <----v       <-----------v        ------------------v
+ *
+ * ( "*" denotes the starting position )
+ *
+ * Therefore, we can only keeps track of only O(1) amount of information as we proceed.
+ */
+public class _335 {
+    public boolean isSelfCrossing(int[] delta) {
+        int[] dx = {0, -1, 0, 1}, dy = {1, 0, -1, 0};
+        int[] x = new int[7], y = x.clone(); // x[6] = y[6] = 0 is implicitly the first point
+        for (int i=0; i<delta.length; i++) {
+            x[i%7] = x[(i+6)%7] + delta[i] * dx[i % 4];
+            y[i%7] = y[(i+6)%7] + delta[i] * dy[i % 4];
+            System.out.println(x[i%7] + " " + y[i%7]);
+            for (int j=2; j+1<7; j++)
+                if (i>=j && _1D(x, i%7, (i-1+7)%7, (i-j+7)%7, (i-j-1+7)%7) && _1D(y, i%7, (i-1+7)%7, (i-j+7)%7, (i-j-1+7)%7)) return true;
+        }
+        return false;
+    }
+
+    private boolean _1D(int[] arr, int i, int j, int x, int y) {
+        return Math.max(arr[i], arr[j]) >= Math.min(arr[x], arr[y]) && Math.max(arr[x], arr[y]) >= Math.min(arr[i], arr[j]);
+    }
+}
---
+++ src/_337.java
@@ -0,0 +1,47 @@
+/**
+ * LeetCode 337 - House Robber III
+ *
+ * Tree DP
+ *
+ * Let x be some node in the tree.
+ * We define x[0] and x[1] as the maximum amount of money the thief can rob
+ * for the subtree rooted at x when x can be robbed and definitely cannot, respectively.
+ *
+ * How to compute x[0]?
+ * We have two choices, i.e., 1) rob node x and 2) don't rob node x.
+ * If it is the former, we get x.val money, but we cannot rob x's immediate children, namely, x.left and x.right.
+ * Therefore, we can get at most x.val + x.left[1] + x.right[1] amount of money.
+ * If it is the latter, we simply skip x and there is no constraint on its children, so x.left[0] + x.right[0] money.
+ * Thus, x[0] = max(x.val + x.left[1] + x.right[1], x.left[0] + x.right[0]) since we always want to maximize the profit.
+ *
+ * How to compute x[1]?
+ * This is just the case 2 of x[0].
+ * x[1] = x.left[0] + x.right[0]
+ *
+ * For the boundary case, if x is null, then x[0] = x[1] = 0.
+ *
+ * The final answer is clearly root[0].
+ * All the calculations can be done by a simple DFS, and therefore, the total runtime is O(n).
+ */
+public class _337 {
+    public int[] dfs(TreeNode root) {
+        if (root == null) return new int[]{0, 0};
+        int[] l = dfs(root.left);
+        int[] r = dfs(root.right);
+        return new int[]{Math.max(l[0] + r[0], root.val + l[1] + r[1]), l[0] + r[0]};
+    }
+
+    public int rob(TreeNode root) {
+        return dfs(root)[0];
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
