f79c8b1c9a32cf94a3276f6b170874102810f431
Yuan Li <lydxlx@gmail.com>
2016-04-19 16:32:35 -0500

    Update _343.java
    I am an idiot.

--- src/_343.java
+++ src/_343.java
@@ -11,7 +11,7 @@
  * Say we can break a sufficiently large number n into any smaller real positive numbers, and we try to calculate which real number generates the largest product.
  * Assume we break n into (n / x) x's, then the product will be x^(n/x), and we want to maximize it.
  *
- * Taking its derivative give us -(x^{n/x} * ln(x) * n / x^2).
+ * Taking its derivative give us n * x^{n/x-2} * (1-ln(x)).
  * The derivative is negative when 0 < x < e, and equal to 0 when x = e, then becomes positive when x > e,
  * which indicates that the product increases as x increases, then reaches its maximum when x = e, then starts dropping.
  *
