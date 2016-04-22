7e14f110f0a903c531a37b478e4b92ff69907514
Yuan Li <lydxlx@gmail.com>
2016-04-22 16:51:52 -0500

    Add 344, Reverse String

---
+++ src/_344.java
@@ -0,0 +1,10 @@
+/**
+ * LeetCode 344 - Reverse String
+ *
+ * A boring problem
+ */
+public class _344 {
+    public String reverseString(String s) {
+        return new StringBuilder(s).reverse().toString();
+    }
+}
