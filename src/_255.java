30ff3c7a86ae5cfc3efa44e4427f04f0fa4f2484
Yuan Li <lixx2100@umn.edu>
2016-09-07 00:43:23 -0500

    Add 255

---
+++ src/_255.java
@@ -0,0 +1,42 @@
+/**
+ * LeetCode 255 - Verify Preorder Sequence in Binary Search Tree
+ *
+ * Construction method
+ * O(n) space & time
+ */
+public class _255 {
+    static class Tree {
+        int val, begin, end;
+        Tree left, right, parent;
+
+        public Tree(int val, int begin, int end) {
+            this.val = val;
+            this.begin = begin;
+            this.end = end;
+        }
+    }
+
+    public boolean verifyPreorder(int[] preorder) {
+        if (preorder.length == 0) return true;
+        Tree root = new Tree(preorder[0], Integer.MIN_VALUE, Integer.MAX_VALUE);
+        for (int i = 1; i < preorder.length; ) {
+            if (root.begin < preorder[i] && preorder[i] < root.val) {
+                if (root.left != null || root.right != null) return false; // If we visit the left branch, make sure it is done before visiting the right one
+                Tree ch = new Tree(preorder[i++], root.begin, root.val);
+                root.left = ch;
+                ch.parent = root;
+                root = ch;
+            } else if (root.val < preorder[i] && preorder[i] < root.end) {
+                if (root.right != null) return false;
+                Tree ch = new Tree(preorder[i++], root.val, root.end);
+                root.right = ch;
+                ch.parent = root;
+                root = ch;
+            } else {
+                if (root.parent == null) return false;
+                root = root.parent;
+            }
+        }
+        return true;
+    }
+}
