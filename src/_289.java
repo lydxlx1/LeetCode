d673a09bc324181ff1ef42180414676ad0d8f08d
Yuan Li <lixx2100@umn.edu>
2016-09-08 00:42:02 -0500

    Update README

---
+++ src/_289.java
@@ -0,0 +1,38 @@
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * LeetCode 289 - Game of Life
+ *
+ * Use the second least significant bit to store the result for next round
+ */
+public class _289 {
+    private int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
+    private int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+    public void gameOfLife(int[][] board) {
+        for (int i = 0; i < board.length; i++)
+            for (int j = 0; j < board[i].length; j++) {
+                int cnt = 0;
+                for (int k = 0; k < 8; k++) {
+                    int x = i + dx[k];
+                    int y = j + dy[k];
+                    if (x >= 0 && x < board.length && y >= 0 && y < board[i].length && (board[x][y] & 1) == 1) cnt++;
+                }
+                if (board[i][j] == 1 && 2 <= cnt && cnt <= 3 || board[i][j] == 0 && cnt == 3) board[i][j] |= 2;
+            }
+        for (int i = 0; i < board.length; i++)
+            for (int j = 0; j < board[i].length; j++)
+                board[i][j] >>= 1;
+    }
+
+    @Test
+    public void test1() {
+        int[][] board = {{1, 1}, {1, 0}};
+        (new _289()).gameOfLife(board);
+        assertEquals(Arrays.deepToString(board), Arrays.deepToString(new int[][]{{1, 1}, {1, 1,}}));
+    }
+}
