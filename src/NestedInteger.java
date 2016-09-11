33262c98cbf8197a3009a0f0a711fde3e0eb0a7a
Yuan Li <lixx2100@umn.edu>
2016-09-11 17:22:36 -0500

    Extract NestedInteger inferface

---
+++ src/NestedInteger.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public interface NestedInteger {
+
+    // @return true if this NestedInteger holds a single integer, rather than a nested list.
+    boolean isInteger();
+
+    // @return the single integer that this NestedInteger holds, if it holds a single integer
+    // Return null if this NestedInteger holds a nested list
+    Integer getInteger();
+
+    // @return the nested list that this NestedInteger holds, if it holds a nested list
+    // Return null if this NestedInteger holds a single integer
+    List<NestedInteger> getList();
+}
--- src/_341.java
+++ src/_341.java
@@ -27,17 +27,3 @@ public class _341 implements Iterator<Integer> {
         return !s.isEmpty();
     }
 }
-
-interface NestedInteger {
-
-    // @return true if this NestedInteger holds a single integer, rather than a nested list.
-    public boolean isInteger();
-
-    // @return the single integer that this NestedInteger holds, if it holds a single integer
-    // Return null if this NestedInteger holds a nested list
-    public Integer getInteger();
-
-    // @return the nested list that this NestedInteger holds, if it holds a nested list
-    // Return null if this NestedInteger holds a single integer
-    public List<NestedInteger> getList();
-}
