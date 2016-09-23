aa361d685ad9389f74f242ad8f2b67c4c748b667
Yuan Li <lixx2100@umn.edu>
2016-09-22 21:29:17 -0500

    Rename class...

--- tests/_403Test.java
+++ tests/_403Test.java
@@ -7,7 +7,7 @@ public class _403Test {
 
     @Test
     public void test() throws Exception {
-        Solution sol = new Solution();
+        _403 sol = new _403();
         assertTrue(sol.canCross(new int[]{0, 1, 3, 5, 6, 8, 12, 17}));
         assertFalse(sol.canCross(new int[]{0, 1, 2, 3, 4, 8, 9, 11}));
         assertFalse(sol.canCross(new int[]{0, 3}));
