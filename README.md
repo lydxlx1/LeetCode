be998488381796bc5589515172b82d554eb79ea1
Yuan Li <lydxlx@gmail.com>
2016-12-24 09:31:06 +0800

    Add 449

--- README.md
+++ README.md
@@ -27,7 +27,7 @@ I will keep updating the list and feel free to share any of your thoughts!
 |454 |[4Sum II][454_t]|\[ [HashMap][454] \]|
 |453 |[Minimum Moves to Equal Array Elements][453_t]|\[ [Solution][453] \]|
 |452 |[Minimum Number of Arrows to Burst Balloons][452_t]|\[ [Greedy via Sweepline][452] \]|
-|449 |[Serialize and Deserialize BST][449_t]|\[ [Pre-order Traversal][449] \]|
+|449 |[Serialize and Deserialize BST][449_t]|\[ [Pre-order Traversal][449] &#124; [Optimized Version][449_1] \]|
 |447 |[Number of Boomerangs][447_t]|\[ [Counting via HashMap][447] \]|
 |446 |[Arithmetic Slices II - Subsequence][446_t]|\[ [Very Fast O(n^3) DP Solution][446] &#124; [O(n^2) DP Solution with Pruning][446_1] \]|
 |444 |[Sequence Reconstruction][444_t]|\[ [Topological Sort][444] \]|
@@ -487,3 +487,4 @@ I will keep updating the list and feel free to share any of your thoughts!
 [472]:    https://github.com/lydxlx1/LeetCode/blob/master/src/_472.java
 [449_t]:   https://leetcode.com/problems/serialize-and-deserialize-bst/
 [449]:    https://github.com/lydxlx1/LeetCode/blob/master/src/_449.java
+[449_1]:    https://github.com/lydxlx1/LeetCode/blob/master/src/_449_1.java
---
+++ src/_449_1.java
@@ -0,0 +1,68 @@
+import java.util.Arrays;
+import java.util.StringJoiner;
+
+/**
+ * LeetCode 449 - Serialize and Deserialize BST
+ * <p>
+ * An optimized version
+ * O(n) - Serialize
+ * O(n log n) - Deserialize
+ */
+public class _449_1 {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        final StringJoiner joiner = new StringJoiner(" ");
+        class Serializer {
+            public void dfs(TreeNode root) {
+                if (root == null) return;
+                joiner.add("" + root.val);
+                dfs(root.left);
+                dfs(root.right);
+            }
+        }
+        new Serializer().dfs(root);
+        return joiner.toString();
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        data = data.trim();
+        if (data.length() <= 0) return null;
+
+        String[] tokens = data.trim().split(" ");
+        int[] preOrder = new int[tokens.length];
+        for (int i = 0; i < preOrder.length; i++)
+            preOrder[i] = Integer.parseInt(tokens[i]);
+        int[] inOrder = preOrder.clone();
+        Arrays.sort(inOrder);
+
+        class Deserializer {
+            public TreeNode dfs(int preL, int preR, int inL, int inR) {
+                if (preR - preL != inR - inL)
+                    throw new IllegalStateException("Inconsistent preorder and inorder sequences.");
+                if (preL > preR) return null;
+                TreeNode root = new TreeNode(preOrder[preL]);
+
+                int left = inL, right = inR;
+                while (left <= right) {
+                    int mid = (left + right) / 2;
+                    if (inOrder[mid] == root.val) {
+                        int leftChildSize = mid - inL;
+                        root.left = dfs(preL + 1, preL + leftChildSize, inL, mid - 1);
+                        root.right = dfs(preL + leftChildSize + 1, preR, mid + 1, inR);
+                        return root;
+                    } else if (inOrder[mid] < root.val) left = mid + 1;
+                    else right = mid - 1;
+
+                }
+                throw new IllegalStateException("Inconsistent preorder and inorder sequences.");
+            }
+        }
+        return new Deserializer().dfs(0, preOrder.length - 1, 0, inOrder.length - 1);
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.deserialize(codec.serialize(root));
