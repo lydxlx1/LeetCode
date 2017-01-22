f3cff48ab20268d343b16b63e17e3e6adbf740ac
Yuan Li <lixx2100@umn.edu>
2017-01-21 22:08:06 -0600

    Update 492

--- src/_492.java
+++ src/_492.java
@@ -6,16 +6,10 @@
 public class _492 {
 
     public int[] constructRectangle(int area) {
-        int[] ans = new int[2];
+        int[] ans = null;
         for (int i = 1; i * i <= area; i++) {
             if (area % i == 0) {
-                int cb = Math.max(i, area / i);
-                int wb = Math.min(i, area / i);
-
-                if (ans[0] == 0 || ans[0] - ans[1] > cb - wb) {
-                    ans[0] = cb;
-                    ans[1] = wb;
-                }
+                ans = new int[]{area / i, i};
             }
         }
         return ans;
