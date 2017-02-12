81e5293b2553895bf55e018685b22fb5d24c6c97
Yuan Li <lixx2100@umn.edu>
2017-02-11 23:01:40 -0600

    Add 515

---
+++ src/_515.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * LeetCode 515 - Find Largest Element in Each Row
+ * <p>
+ * DFS approach
+ */
+public class _515 {
+
+    public int[] findValueMostElement(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        dfs(root, 0, list);
+        return list.stream().mapToInt(u -> u).toArray();
+    }
+
+    private void dfs(TreeNode root, int d, List<Integer> list) {
+        if (root == null) return;
+        if (d >= list.size()) list.add(root.val);
+        else list.set(d, Math.max(list.get(d), root.val));
+        dfs(root.left, d + 1, list);
+        dfs(root.right, d + 1, list);
+    }
+}
+
+
