8047c13e56b477577bcd62c8fe03e67d9cc68fa4
Yuan Li <lydxlx@gmail.com>
2017-05-18 12:10:23 -0500

    Add Point class

---
+++ src/Point.java
@@ -0,0 +1,19 @@
+public class Point {
+    int x;
+    int y;
+
+    Point() {
+        x = 0;
+        y = 0;
+    }
+
+    Point(int a, int b) {
+        x = a;
+        y = b;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + "," + y + ")";
+    }
+}
