3643616486af94be6f8586f5f2569971a3877f3f
Yuan Li <lydxlx@gmail.com>
2018-08-19 03:16:31 -0700

    Update _891.java

--- src/_891.java
+++ src/_891.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
  *   - As a takeway, the same trick applies to any objective function that is insensitive to ordering.
  *   - Ex: sum, prod, average, mean, min-gap.
  * - Now, consider the original array A and its sorted version A'. Let n denote |A|.
- *   Let P(A) be the set containing all non-empty sub-sequence of A. Clearly, |P(A)| = 2^n - 1.
+ *   Let P(A) be the set containing all non-empty sub-sequences of A. Clearly, |P(A)| = 2^n - 1.
  *   Similarly, we define P(A') and |P(A')| = 2^n - 1.
  * - To show the answer of A and A' are the same, it suffices to find a bi-jection between P(A) and P(A'), which clearly exists.
  *
@@ -24,7 +24,7 @@ import java.util.Arrays;
  * Let f[i] denote the # of sub-sequence ending at A[i]. Then every such sub-sequence contributes A[i] to F(A).
  * Therefore, F(A) = sum(f[i] * A[i]), 0 <= i < n.
  *
- * Finally, f[i] is clearly equal to 2^i, as A is sorted. (Note, index-i starts from 0).
+ * Finally, f[i] is equal to 2^i, as A is sorted. (Note, index-i starts from 0).
  */
 public class _891 {
 
