ad11ac29ed3f720dbbba7fad31f86f4952bdca6e
Yuan Li <4112003+lydxlx1@users.noreply.github.com>
2023-01-16 01:38:25 -0800

    Insert the correct code of 2536

--- src/_2536 Increment Submatrices by One.cpp
+++ src/_2536 Increment Submatrices by One.cpp
@@ -1,34 +1,42 @@
 /**
- * LeetCode 2537 - Count the Number of Good Subarrays
+ * LeetCode 2536 - Increment Submatrices by One
  *
- * Sliding window approach
- * O(n) time / space complexity
+ * 2D suffix / prefix sum
+ * Inclusion & Exclusion
+ * Time complexity: O(Q + N^2)
+ * Space complexity: O(N^2)
  */
 
 class Solution {
 public:
-  long long countGood(vector<int> &nums, int k) {
-    long long ans = 0;
-    long long cur_good_pair = 0;
-    unordered_map<int, long long> counter;
-
-    // Enumerate each right boundary of the sliding window.
-    // Find the smallest window [left, right] to satisfy the "good" condition.
-    // Then, every subarray with left boundary no bigger than `left` will all satisfy the condition.
-    for (int right = 0, left = 0; right < nums.size(); right++) {
-      cur_good_pair += counter[nums[right]];
-      counter[nums[right]]++;
-
-      while (left <= right && cur_good_pair - (counter[nums[left]] - 1) >= k) {
-        counter[nums[left]]--;  // Do this step first to exclude the number itself
-        cur_good_pair -= counter[nums[left]];
-        left++;
+    vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
+      vector<vector<int>> mat(n, vector<int>(n, 0));
+      
+      // Here, mat[x][y] means we want to add mat[x][y] to all elements in the submatrics (or quadrant)
+      // with top-left corner being (x, y). 
+      // Then, each submatrix update can be splitted into four quadrant updates, using inclusion-exclusion.
+      // This is also like a 2D suffix sum.
+      for (auto& query : queries) {
+        int x = query[0], y = query[1], xx = query[2], yy = query[3];
+        mat[x][y]++;
+        if (xx + 1 < n)
+          mat[xx+1][y]--;
+        if (yy + 1 < n)
+          mat[x][yy+1]--;
+        if (xx + 1 < n && yy + 1 < n)
+          mat[xx+1][yy+1]++;
       }
-      if (cur_good_pair >= k) {
-        // [0, 1, ... left] are good left boundary candidates w.r.t. right
-        ans += left + 1;
+      
+      // Now, the actual value of final mat[x][y] is equal to the quadrant sum with bottom-right corner being (x, y),
+      // which is again a 2D prefix sum and can be handled inclusion-exclusion too.
+      for (int i=0; i<n; i++) {
+        for (int j=0; j<n; j++) {
+          int& res = mat[i][j];
+          res += i - 1 >= 0 ? mat[i-1][j] : 0;
+          res += j - 1 >= 0 ? mat[i][j-1] : 0;
+          res -= i - 1 >= 0 && j - 1 >= 0 ? mat[i-1][j-1] : 0;
+        }
       }
+      return mat;
     }
-    return ans;
-  }
 };
